b77d0dcbfc0e5659b4cb61efa8fe63afec400598
Biranchi <susantabehera@Susantas-MacBook-Pro-2.local>
2013-09-07 21:45:47 +0800

    Files updated

--- MDDParking/PlaceDetailViewController.h
+++ MDDParking/PlaceDetailViewController.h
@@ -8,6 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
-@interface PlaceDetailViewController : UIViewController
+@class MDDParkingSpot;
+@interface PlaceDetailViewController : UIViewController {
+  
+}
+
+@property (strong, nonatomic) MDDParkingSpot *parkingSpotObj;
+@property (weak, nonatomic) IBOutlet UILabel *parkingSpotNameLbl;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *weekDaysSegmentedControl;
+@property (weak, nonatomic) IBOutlet UILabel *weekDaysRateLbl;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *weekEndSegmentedControl;
+@property (weak, nonatomic) IBOutlet UILabel *weekEndRateLbl;
+
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *publicHolidaySegmentedControl;
+@property (weak, nonatomic) IBOutlet UILabel *publicHolidayRateLbl;
 
 @end
--- MDDParking/PlaceDetailViewController.m
+++ MDDParking/PlaceDetailViewController.m
@@ -9,6 +9,7 @@
 #import "PlaceDetailViewController.h"
 #import <MapKit/MapKit.h>
 #import "AddNewPlaceViewController.h"
+#import "MDDParkingSpot.h"
 
 @interface PlaceDetailViewController ()
 //- (void)configureView;
@@ -16,26 +17,10 @@
 
 @implementation PlaceDetailViewController
 
+@synthesize parkingSpotObj;
+
 #pragma mark - Managing the detail item
 
-//- (void)setDetailItem:(id)newDetailItem
-//{
-//    if (_detailItem != newDetailItem) {
-//        _detailItem = newDetailItem;
-//        
-//        // Update the view.
-//        [self configureView];
-//    }
-//}
-//
-//- (void)configureView
-//{
-//    // Update the user interface for the detail item.
-//
-//	if (self.detailItem) {
-//	    self.detailDescriptionLabel.text = [self.detailItem description];
-//	}
-//}
 
 - (void)viewDidLoad
 {
@@ -43,8 +28,12 @@
 	// Do any additional setup after loading the view, typically from a nib.
 	//[self configureView];
 	self.navigationItem.title = @"Details";
-  
-  
+
+	
+  if(self.parkingSpotObj){
+	self.parkingSpotNameLbl.text = self.parkingSpotObj.name;
+	
+  }
 }
 
 
